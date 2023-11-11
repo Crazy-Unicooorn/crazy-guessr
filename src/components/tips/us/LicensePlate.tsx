a9fadc503bcded05d35f96dd9a0f9396337b40d6
Crazy Unicorn <darkmen.michel@gmail.com>
2023-11-11 19:06:03 +0100

    new license plates

--- src/components/tips/us/LicensePlate.tsx
+++ src/components/tips/us/LicensePlate.tsx
@@ -15,8 +15,15 @@ import Nevada from "../../../assets/img/us/plates/Nevada.jpg";
 import Oklahoma from "../../../assets/img/us/plates/Oklahoma.jpg";
 import Iowa1 from "../../../assets/img/us/plates/Iowa.jpg";
 import Iowa2 from "../../../assets/img/us/plates/Iowa2.jpg";
+import NewMexico from "../../../assets/img/us/plates/NewMexico.jpg";
+import NewMexico1 from "../../../assets/img/us/plates/NewMexico1.jpg";
+import NewMexico2 from "../../../assets/img/us/plates/NewMexico2.jpg";
+import NewMexico3 from "../../../assets/img/us/plates/NewMexico3.jpg";
+import NorthDakota from "../../../assets/img/us/plates/NorthDakota.jpg";
 
 import Alabama1 from "../../../assets/img/us/plates/blurred/alabama1.jpg";
+import Alabama2 from "../../../assets/img/us/plates/blurred/alabama2.jpg";
+import Alabama3 from "../../../assets/img/us/plates/blurred/alabama3.jpg";
 import Minnesota1 from "../../../assets/img/us/plates/blurred/Minnesota1.jpg";
 import MississippiBlurred1 from "../../../assets/img/us/plates/blurred/Mississippi1.jpg";
 import MississippiBlurred2 from "../../../assets/img/us/plates/blurred/Mississippi2.jpg";
@@ -33,6 +40,23 @@ import Oklahoma4 from "../../../assets/img/us/plates/blurred/Oklahoma4.jpg";
 import IowaBlurred1 from "../../../assets/img/us/plates/blurred/Iowa1.jpg";
 import IowaBlurred2 from "../../../assets/img/us/plates/blurred/Iowa2.jpg";
 import IowaBlurred3 from "../../../assets/img/us/plates/blurred/Iowa3.jpg";
+import NewMexicoBlurredA from "../../../assets/img/us/plates/blurred/NewMexicoA.jpg";
+import NewMexicoBlurredB from "../../../assets/img/us/plates/blurred/NewMexicoB.jpg";
+import NewMexicoBlurredC from "../../../assets/img/us/plates/blurred/NewMexicoC.jpg";
+import NewMexicoBlurredD from "../../../assets/img/us/plates/blurred/NewMexicoD.jpg";
+import NewMexicoBlurred1A from "../../../assets/img/us/plates/blurred/NewMexico1A.jpg";
+import NewMexicoBlurred1B from "../../../assets/img/us/plates/blurred/NewMexico1B.jpg";
+import NewMexicoBlurred2A from "../../../assets/img/us/plates/blurred/NewMexico2A.jpg";
+import NewMexicoBlurred2B from "../../../assets/img/us/plates/blurred/NewMexico2B.jpg";
+import NewMexicoBlurred2C from "../../../assets/img/us/plates/blurred/NewMexico2C.jpg";
+import NewMexicoBlurred2D from "../../../assets/img/us/plates/blurred/NewMexico2D.jpg";
+import NewMexicoBlurred3A from "../../../assets/img/us/plates/blurred/NewMexico3A.jpg";
+import NewMexicoBlurred3B from "../../../assets/img/us/plates/blurred/NewMexico3B.jpg";
+import NorthDakotaBlurred1 from "../../../assets/img/us/plates/blurred/NorthDakota.jpg";
+import NorthDakotaBlurred2 from "../../../assets/img/us/plates/blurred/NorthDakota2.jpg";
+import NorthDakotaBlurred3 from "../../../assets/img/us/plates/blurred/NorthDakota3.jpg";
+import NorthDakotaBlurred4 from "../../../assets/img/us/plates/blurred/NorthDakota4.jpg";
+import NorthDakotaBlurred5 from "../../../assets/img/us/plates/blurred/NorthDakota5.jpg";
 
 function LicensePlate() {
   const StyleBack = styled.div`
@@ -41,18 +65,27 @@ function LicensePlate() {
     justify-content: center;
     align-items: center;
     gap: 1rem;
-    height: 6rem;
+    max-height: 8rem;
+    height: 100%;
   `;
-  const styleImage = { maxHeight: "100%", borderRadius: "0.5rem" };
+  const styleImage = { height: "100%", borderRadius: "0.5rem" };
+
+  function img(src: string) {
+    return <img src={src} alt="A blurred license plate" style={styleImage} />;
+  }
+
   const cards: Card[] = [
     {
-      front: <img src={Alabama1} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Alabama} alt="A license plate" style={styleImage} />
-          Alabama
-        </StyleBack>
-      ),
+      front: img(Alabama1),
+      back: <StyleBack>{img(Alabama)} Alabama</StyleBack>,
+    },
+    {
+      front: img(Alabama2),
+      back: <StyleBack>{img(Alabama)} Alabama</StyleBack>,
+    },
+    {
+      front: img(Alabama3),
+      back: <StyleBack>{img(Alabama)} Alabama</StyleBack>,
     },
     // { front: "Alaska", back: "No" },
     // { front: "Arizona", back: "Yes" },
@@ -68,31 +101,16 @@ function LicensePlate() {
     // { front: "Illinois", back: "Yes" },
     // { front: "Indiana", back: "Yes" },
     {
-      front: <img src={IowaBlurred1} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Iowa1} alt="A license plate" style={styleImage} />
-          Iowa
-        </StyleBack>
-      ),
+      front: img(IowaBlurred1),
+      back: <StyleBack>{img(Iowa1)} Iowa</StyleBack>,
     },
     {
-      front: <img src={IowaBlurred2} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Iowa2} alt="A license plate" style={styleImage} />
-          Iowa
-        </StyleBack>
-      ),
+      front: img(IowaBlurred2),
+      back: <StyleBack>{img(Iowa2)} Iowa</StyleBack>,
     },
     {
-      front: <img src={IowaBlurred3} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Iowa2} alt="A license plate" style={styleImage} />
-          Iowa
-        </StyleBack>
-      ),
+      front: img(IowaBlurred3),
+      back: <StyleBack>{img(Iowa2)} Iowa</StyleBack>,
     },
     // { front: "Kansas", back: "Yes" },
     // { front: "Kentucky", back: "Yes" },
@@ -102,130 +120,131 @@ function LicensePlate() {
     // { front: "Massachusetts", back: "Yes" },
     // { front: "Michigan", back: "Yes" },
     {
-      front: <img src={Minnesota1} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Minnesota} alt="A license plate" style={styleImage} />
-          Minnesota
-        </StyleBack>
-      ),
+      front: img(Minnesota1),
+      back: <StyleBack>{img(Minnesota)} Minnesota</StyleBack>,
     },
     {
-      front: <img src={MississippiBlurred1} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Mississippi1} alt="A license plate" style={styleImage} />
-          Mississippi
-        </StyleBack>
-      ),
+      front: img(MississippiBlurred1),
+      back: <StyleBack>{img(Mississippi1)} Mississippi</StyleBack>,
     },
     {
-      front: <img src={MississippiBlurred2} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Mississippi2} alt="A license plate" style={styleImage} />
-          Mississippi
-        </StyleBack>
-      ),
+      front: img(MississippiBlurred2),
+      back: <StyleBack>{img(Mississippi2)} Mississippi</StyleBack>,
     },
     {
-      front: <img src={MissouriBlurred1} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Missouri1} alt="A license plate" style={styleImage} />
-          Missouri
-        </StyleBack>
-      ),
+      front: img(MissouriBlurred1),
+      back: <StyleBack>{img(Missouri1)} Missouri</StyleBack>,
     },
     {
-      front: <img src={MissouriBlurred2} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Missouri2} alt="A license plate" style={styleImage} />
-          Missouri
-        </StyleBack>
-      ),
+      front: img(MissouriBlurred2),
+      back: <StyleBack>{img(Missouri2)} Missouri</StyleBack>,
     },
     // { front: "Montana", back: "No" },
     // { front: "Nebraska", back: "Yes" },
     {
-      front: <img src={Nevada1} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Nevada} alt="A license plate" style={styleImage} />
-          Nevada
-        </StyleBack>
-      ),
+      front: img(Nevada1),
+      back: <StyleBack>{img(Nevada)} Nevada</StyleBack>,
     },
     {
-      front: <img src={Nevada2} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Nevada} alt="A license plate" style={styleImage} />
-          Nevada
-        </StyleBack>
-      ),
+      front: img(Nevada2),
+      back: <StyleBack>{img(Nevada)} Nevada</StyleBack>,
     },
     {
-      front: <img src={Nevada3} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Nevada} alt="A license plate" style={styleImage} />
-          Nevada
-        </StyleBack>
-      ),
+      front: img(Nevada3),
+      back: <StyleBack>{img(Nevada)} Nevada</StyleBack>,
     },
     {
-      front: <img src={Nevada4} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Nevada} alt="A license plate" style={styleImage} />
-          Nevada
-        </StyleBack>
-      ),
+      front: img(Nevada4),
+      back: <StyleBack>{img(Nevada)} Nevada</StyleBack>,
     },
     // { front: "New Hampshire", back: "Yes" },
     // { front: "New Jersey", back: "Yes" },
-    // { front: "New Mexico", back: "Yes" },
+    {
+      front: img(NewMexicoBlurredA),
+      back: <StyleBack> {img(NewMexico)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurredB),
+      back: <StyleBack> {img(NewMexico)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurredC),
+      back: <StyleBack> {img(NewMexico)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurredD),
+      back: <StyleBack> {img(NewMexico)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred1A),
+      back: <StyleBack> {img(NewMexico1)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred1B),
+      back: <StyleBack> {img(NewMexico1)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred2A),
+      back: <StyleBack> {img(NewMexico2)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred2B),
+      back: <StyleBack> {img(NewMexico2)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred2C),
+      back: <StyleBack> {img(NewMexico2)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred2D),
+      back: <StyleBack> {img(NewMexico2)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred3A),
+      back: <StyleBack> {img(NewMexico3)} New Mexico</StyleBack>,
+    },
+    {
+      front: img(NewMexicoBlurred3B),
+      back: <StyleBack> {img(NewMexico3)} New Mexico</StyleBack>,
+    },
     // { front: "New York", back: "Yes" },
     // { front: "North Carolina", back: "Yes" },
-    // { front: "North Dakota", back: "Yes" },
+    {
+      front: img(NorthDakotaBlurred1),
+      back: <StyleBack> {img(NorthDakota)} North Dakota</StyleBack>,
+    },
+    {
+      front: img(NorthDakotaBlurred2),
+      back: <StyleBack> {img(NorthDakota)} North Dakota</StyleBack>,
+    },
+    {
+      front: img(NorthDakotaBlurred3),
+      back: <StyleBack> {img(NorthDakota)} North Dakota</StyleBack>,
+    },
+    {
+      front: img(NorthDakotaBlurred4),
+      back: <StyleBack> {img(NorthDakota)} North Dakota</StyleBack>,
+    },
+    {
+      front: img(NorthDakotaBlurred5),
+      back: <StyleBack> {img(NorthDakota)} North Dakota</StyleBack>,
+    },
     // { front: "Ohio", back: "Yes" },
     {
-      front: <img src={Oklahoma1} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Oklahoma} alt="A license plate" style={styleImage} />
-          Oklahoma
-        </StyleBack>
-      ),
+      front: img(Oklahoma1),
+      back: <StyleBack>{img(Oklahoma)} Oklahoma</StyleBack>,
     },
     {
-      front: <img src={Oklahoma2} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Oklahoma} alt="A license plate" style={styleImage} />
-          Oklahoma
-        </StyleBack>
-      ),
+      front: img(Oklahoma2),
+      back: <StyleBack>{img(Oklahoma)} Oklahoma</StyleBack>,
     },
     {
-      front: <img src={Oklahoma3} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Oklahoma} alt="A license plate" style={styleImage} />
-          Oklahoma
-        </StyleBack>
-      ),
+      front: img(Oklahoma3),
+      back: <StyleBack>{img(Oklahoma)} Oklahoma</StyleBack>,
     },
     {
-      front: <img src={Oklahoma4} alt="A blurred license plate" style={styleImage} />,
-      back: (
-        <StyleBack>
-          <img src={Oklahoma} alt="A license plate" style={styleImage} />
-          Oklahoma
-        </StyleBack>
-      ),
+      front: img(Oklahoma4),
+      back: <StyleBack>{img(Oklahoma)} Oklahoma</StyleBack>,
     },
     // { front: "Oregon", back: "Yes" },
     // { front: "Pennsylvania", back: "Yes" },
