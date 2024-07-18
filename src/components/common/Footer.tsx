0245c01e368e103b8b5e16222bfb03b3285785e6
Crazy Unicorn <darkmen.michel@gmail.com>
2024-07-18 11:02:36 +0200

    UI fix

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,6 +1,6 @@
 function Footer() {
   return (
-    <div>
+    <div style={{ margin: "4rem 0" }}>
       Looking for more tips?{" "}
       <a href="https://github.com/Crazy-Unicooorn/crazy-guessr/issues" target="_blank" rel="noopener noreferrer">
         Suggest on Github!
